0ed637b593b7c092fa6ec9a38f1f2d050bc63457
JX_Tee <jinxintee@gmail.com>
2025-11-12 13:38:32 +0800

    Fix: docker‘s enviroment Frontend and backend connections

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Vite 預設 port
+    origin: ['http://localhost:5173', 'http://frontend:5173'], // Vite 預設 port (本地和 Docker)
     credentials: true,
   });
   await app.listen(3000);
