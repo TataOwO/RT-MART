f49de4d00f9a763cf79061853ec6de61deb68a2b
chuni <86214112+xinchen-8@users.noreply.github.com>
2025-12-18 17:36:41 +0800

    Add: Product image api

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -4,7 +4,8 @@ import * as streamifier from 'streamifier';
 
 @Injectable()
 export class CloudinaryService {
-  uploadImage(file: Express.Multer.File): Promise<string> {
+
+  uploadImage(file: Express.Multer.File): Promise<{ url: string; publicId: string }> {
     return new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
         { folder: 'products' },
@@ -12,11 +13,21 @@ export class CloudinaryService {
           if (error) return reject(error);
           if (!result) return reject(new Error('Cloudinary result is undefined'));
 
-          resolve(result.secure_url);
+          resolve({
+            url: result.secure_url,
+            publicId: result.public_id
+          });
         },
       );
       streamifier.createReadStream(file.buffer).pipe(uploadStream);
     });
   }
 
+  async deleteImage(publicId: string): Promise<void> {
+    try {
+      await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+      throw new Error(`Failed to delete image: ${error.message}`);
+    }
+  }
 }
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -25,6 +25,7 @@ import { UserRole } from '../users/entities/user.entity';
 import { AuthTokenResponseDto } from '@/auth/dto/auth-response.dto';
 import type { AuthRequest } from '../common/types';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { SortImagesDto, UpdateSortedImagesDto } from './dto/upate-sortedImages.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -58,22 +59,56 @@ export class ProductsController {
     return await this.productsService.findOne(id);
   }
 
-  // @Patch(':id/store/:storeId')
-  // @UseGuards(JwtAccessGuard, RolesGuard)
-  // @Roles(UserRole.SELLER)
-  // async update(
-  //   @Param('id') id: string,
-  //   @Param('storeId') storeId: string,
-  //   @Body() updateDto: UpdateProductDto,
-  // ) {
-  //   return await this.productsService.update(id, storeId, updateDto);
-  // }
-
-  @Delete(':id/store/:storeId')
-  @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles(UserRole.SELLER)
-  async remove(@Param('id') id: string, @Param('storeId') storeId: string) {
-    await this.productsService.remove(id, storeId);
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Patch(':id')
+  async update(
+    @Req() req: AuthRequest,
+    @Param('id') id: string,
+    @Body() updateDto: UpdateProductDto,
+  ) {
+    return await this.productsService.update(req.user.userId, id, updateDto);
+  }
+
+  @Roles(UserRole.SELLER)
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @UseInterceptors(FilesInterceptor('images'))
+  @Post(':id/images')
+  async addImages(
+    @Req() req: AuthRequest,
+    @Param('id') id: string,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    return await this.productsService.addImages(req.user.userId, id, files);
+  }
+  
+  @Roles(UserRole.SELLER)
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Patch(':id/images/sort')
+  async sortImages(
+    @Req() req: AuthRequest,
+    @Param('id') id: string,
+    @Body() sortImages: SortImagesDto,
+  ) {
+    return await this.productsService.sortImages(req.user.userId, id, sortImages);
+  }
+
+  @Roles(UserRole.SELLER)
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Delete(':id/images/:imageId')
+  async deleteImage(
+    @Req() req: AuthRequest,
+    @Param('id') id: string,
+    @Param('imageId') imageId: string,
+  ) {
+    return await this.productsService.deleteImage(req.user.userId, id, imageId);
+  }
+
+  @Roles(UserRole.SELLER)
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Delete(':id')
+  async remove(@Req() req: AuthRequest, @Param('id') id: string) {
+    await this.productsService.remove(req.user.userId, id);
     return { message: 'Product deleted successfully' };
   }
 
