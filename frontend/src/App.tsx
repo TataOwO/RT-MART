2f9bd981040f5a8a0a064f7a0459a0aaebddd012
JX_Tee <jinxintee@gmail.com>
2025-12-25 16:06:36 +0800

    Feat(app): Update seller route access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -131,7 +131,7 @@ function AppContent() {
           <Route
             path="/seller/*"
             element={
-              <ProtectedRoute excludeRoles={['buyer', 'admin']}>
+              <ProtectedRoute excludeRoles={['admin']}>
                 <SellerCenter />
               </ProtectedRoute>
             }
